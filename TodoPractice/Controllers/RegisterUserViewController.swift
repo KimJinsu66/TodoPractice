bdafc61e712ff73b4cec9d0d2777bb521663a7a7
KimJinsu66 <j_kim@ga-tech.co.jp>
2023-10-02 00:01:02 +0900

    유저 등록 화면 디자인을 작성 모달을 작성하고 닫기 버튼이 가능하도록 구현

---
+++ TodoPractice/Controllers/RegisterUserViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterUserViewController.swift
+//  TodoPractice
+//
+//  Created by j_kim on 2023/10/01.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func exitButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
